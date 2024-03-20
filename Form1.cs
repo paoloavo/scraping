5ccaecb90817f18ef399e7c159c092a171c0c844
paoloavo <avogadro.paolo.studente@itispaleocapa.it>
2024-03-20 18:18:09 +0100

    part.1

---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.Text.RegularExpressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Net.WebRequestMethods;
+
+namespace scraping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private struct programmazione
+        {
+            public string canale;
+            public string titolo;
+            public string orario;
+            public string descrizione;
+
+            public override string ToString()
+            {
+                return $"{orario} * {canale} * {titolo} * {descrizione}";
+            }
+
+
+        }
+        List<programmazione> elencoProgrammi = new List<programmazione>();
+        programmazione programmaAttuale;
+
+        private int num = 0;
+
+
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var luogo = textBox1.Text;
+            string url = $"https://www.dovesciare.it/webcam/{luogo}";
+
+            label1.Text = url;
+
+
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.AllowAutoRedirect = true;
+            browser.AllowMetaRedirect = true;
+
+            WebPage webpage = await browser.NavigateToPageAsync(new Uri(url));
+
+            var webcam = webpage.Html.OwnerDocument.DocumentNode.CssSelect("div.row").ToList()[1];
+            var urlImg = webpage.Html.OwnerDocument.DocumentNode.CssSelect("section.col-sm-12");
+             var region = urlImg.CssSelect("div.region");
+        var webc = region.CssSelect("article.webcam");
+        var jumbotron = webc.CssSelect("div.jumbotron");
+        var container = jumbotron.CssSelect("div.container");
+    var row = container.CssSelect("div.row");
+    var blocco = row.CssSelect("div#webcam_intro_left").ToList();
+
+    //var webcamintro = blocco.CssSelect("");
+    //var row2 = webcamintro.CssSelect("div.row");
+    //var col = row2.CssSelect("div.col-xs-12");
+    var img = blocco.CssSelect("img.img-responsive").First().GetAttributeValue("src");
+            var img1 = $"https://www.dovesciare.it{img}";
+            var request = WebRequest.Create(img1);
+
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                pictureBox1.Image = Bitmap.FromStream(stream);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
