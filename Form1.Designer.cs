e6077a7c477dca3b05180d42d1089cce0935640f
paoloavo <avogadro.paolo.studente@itispaleocapa.it>
2024-04-20 11:22:59 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace scraping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(405, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(572, 365);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 86);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(405, 31);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1125, 840);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 536);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 766);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.Text.RegularExpressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Net.WebRequestMethods;
+using ImageResizer;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing.Imaging;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+
+namespace scraping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private struct programmazione
+        {
+            public string canale;
+            public string titolo;
+            public string orario;
+            public string descrizione;
+
+            public override string ToString()
+            {
+                return $"{orario} * {canale} * {titolo} * {descrizione}";
+            }
+
+
+        }
+        List<programmazione> elencoProgrammi = new List<programmazione>();
+        programmazione programmaAttuale;
+
+        private int num = 0;
+
+
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var luogo = textBox1.Text;
+            string url = $"https://www.dovesciare.it/webcam/{luogo}";
+
+            label1.Text = url;
+
+
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.AllowAutoRedirect = true;
+            browser.AllowMetaRedirect = true;
+
+            WebPage webpage = await browser.NavigateToPageAsync(new Uri(url));
+
+            var webcam = webpage.Html.OwnerDocument.DocumentNode.CssSelect("div.row").ToList()[1];
+            var urlImg = webpage.Html.OwnerDocument.DocumentNode.CssSelect("section.col-sm-12");
+            var region = urlImg.CssSelect("div.region");
+            var webc = region.CssSelect("article.webcam");
+            var jumbotron = webc.CssSelect("div.jumbotron");
+            var container = jumbotron.CssSelect("div.container");
+            var row = container.CssSelect("div.row");
+            var blocco = row.CssSelect("div#webcam_intro_left").ToList();
+            var img = blocco.CssSelect("img.img-responsive").First().GetAttributeValue("src");
+            var img1 = $"https://www.dovesciare.it{img}";
+            var request = WebRequest.Create(img1);
+
+            try
+            {
+
+                using (WebClient webClient = new WebClient())
+                using (MemoryStream originalStream = new MemoryStream(webClient.DownloadData(img1)))
+                {
+
+                    using (Image originalImage = Image.FromStream(originalStream))
+                    {
+
+                        int newWidth = 400;
+                        int newHeight = 400;
+
+                        // Resize the image
+                        using (Image resizedImage = new Bitmap(newWidth, newHeight))
+                        using (Graphics graphics = Graphics.FromImage(resizedImage))
+                        {
+                            graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                            graphics.DrawImage(originalImage, 0, 0, newWidth, newHeight);
+
+
+                            Bitmap bitmap = new Bitmap(resizedImage);
+
+
+                            pictureBox1.Image = bitmap;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+
+        {
+            var luogo = textBox1.Text;
+            string url = $"https://www.3bmeteo.com/meteo/{luogo}";
+
+
+
+
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.AllowAutoRedirect = true;
+            browser.AllowMetaRedirect = true;
+
+
+
+
+            // Inizializza il WebDriver di Chrome
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized"); // Massimizza la finestra del browser
+            IWebDriver driver = new ChromeDriver(options);
+
+            // Naviga verso la pagina web
+            driver.Navigate().GoToUrl($"https://www.3bmeteo.com/meteo/{luogo}");
+
+
+
+            try
+            {
+                // Trova il bottone tramite il selettore CSS, XPath o altri metodi di localizzazione
+                var button = driver.FindElement(By.CssSelector("div#iubenda-cs-banner"));
+                var button1 = button.FindElement(By.CssSelector("div.iubenda-cs-container"));
+                var button2 = button1.FindElement(By.CssSelector("div.iubenda-cs-content"));
+                var button3 = button2.FindElement(By.CssSelector("div.iubenda-cs-rationale"));
+                var button4 = button3.FindElement(By.CssSelector("div.iubenda-cs-opt-group"));
+                var button5 = button4.FindElement(By.CssSelector("div.iubenda-cs-opt-group-consent"));
+                var button6 = button5.FindElement(By.CssSelector("button.iubenda-cs-accept-btn"));
+
+
+                // Esegui il clic sul bottone
+                button6.Click();
+
+                IWebElement divElement = driver.FindElement(By.XPath("//div[@id='wrapper']")); // Cambia 'tuo_id_div' con l'id effettivo del tuo div
+                IWebElement child = divElement.FindElement(By.XPath(".//section[@id='main']")); // Cambia 'tuo_id_div' con l'id effettivo del tuo div
+                IWebElement child1 = child.FindElement(By.XPath(".//div[@class='box noMarg']")); // Cambia 'tuo_id_div' con l'id effettivo del tuo div
+
+
+                // Ora puoi catturare uno screenshot del quinto figlio div
+                Screenshot screenshot = ((ITakesScreenshot)child1).GetScreenshot();
+                screenshot.SaveAsFile($"div_screenshot{num}.png");
+            }
+
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine("Uno dei div non è stato trovato: " + ex.Message);
+            }
+
+            finally
+            {
+                // Chiudi il WebDriver
+                driver.Quit();
+                pictureBox2.Image = Image.FromFile($"div_screenshot{num}.png");
+
+                num++;
+            }
+
+
+
+        }
+    }
+}
